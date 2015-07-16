ff039bc15c06a031fdc930579cd66a1efba6d67f
extra <extra0@mail.ru>
2015-07-16 23:05:39 +0300

    частично сделана шапка

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
-	//
+	//открываем телефоны в шапке
+	$('.header__phone-link').click(function(){
+		$(this).parent().hide();
+		$('.header__phone-image').hide();
+		$(this).parent().parent().find('.hide').slideDown(500);
+	});
 
 });
