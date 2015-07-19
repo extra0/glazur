db1212f455ef9e2a73970bdf1fa653e2f26ab246
extra <extra0@mail.ru>
2015-07-19 15:36:54 +0300

    рабочая версия корзины

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 	//открываем телефоны в шапке
-	$('.header__phone-link').click(function(){
+	$('.header__phone-link').click(function() {
 		$(this).parent().hide();
 		$('.header__phone-image').hide();
 		$(this).parent().parent().find('.hide').slideDown(500);
@@ -9,7 +9,7 @@ $(document).ready(function(){
 	});
 
 	// выпадающее меню
-	$('.menu__item').hover(function(){
+	$('.menu__item').hover(function() {
 		$(this).find('.dropdown-menu__list').slideToggle(100);
 	});
 
@@ -22,21 +22,21 @@ $(document).ready(function(){
 	});
 
 	// замена изображений по ховеру
-	$('.goods__item').hover(function(){
+	$('.goods__item').hover(function() {
 		$(this).find(".goods__image").toggleClass("_not-active");
 	});
 
-	$('.goods__bestseller').hover(function(){
+	$('.goods__bestseller').hover(function() {
 		$(this).find(".goods__bestseller-image").toggleClass("_not-active");
 	});
 
 	// вращаем иконку в блоке интересного в футере
-	$('.intresting__changer').hover(function(){
+	$('.intresting__changer').hover(function() {
 		$(this).toggleClass('_hover');
 	});
 
 	// открываем модальное окно входа / регистарции
-	$('.enter').click(function(){
+	$('.enter').click(function() {
 		$('body').prepend('<div class="wrap"></div>');
 		$('.modal__window').slideDown(500);
 		return false;
@@ -44,17 +44,20 @@ $(document).ready(function(){
 
 	// закрываем модальное окно по клику вне его области
 	$(document).click(function(event) {
-	    if ($(event.target).closest(".modal__window").length) return;
-	            $('.modal__window').slideUp(500);
-	            $('.wrap').remove();
-	            indexClick = 0;
-	    event.stopPropagation();
+		if ($(event.target).closest(".modal__window").length) return;
+		$('.modal__window').slideUp(500);
+		$('.wrap').remove();
+		indexClick = 0;
+		event.stopPropagation();
 	});
 
 	// вызываем табы
 	$('#tabs').tabs({
 		active: 2,
-		show: { effect: "fade", duration: 800 }
+		show: {
+			effect: "fade",
+			duration: 800
+		}
 	});
 
 	// маска на телефоны
@@ -63,13 +66,30 @@ $(document).ready(function(){
 	//  стилизация чекбоксов и селектов
 	$('.checkbox').styler();
 
-	// меняем блоки по клику в интересном
-	var total = $('.intresting__inner').length,
-		index = $('.intresting__inner').index();
-
-	$('.intresting__changer').click(function(){
-
-		return false;
+	// полет товара в корзину
+	$(".goods__button._cart").click(function() {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 1000)
+		setTimeout(function() {
+			$('.cart__image').addClass('_full');
+		}, 1500)
+		$(this).children('img')
+			.clone()
+			.css({
+				'position': 'absolute',
+				'z-index': '100'
+			})
+			.appendTo(this)
+			.animate({
+				opacity: 0.5,
+				marginTop: $('.cart__image').offset().top - $(this).children('img').offset().top,
+				marginLeft: $('.cart__image').offset().left - $(this).children('img').offset().left,
+				width: 50,
+				height: 50
+			}, 860, function() {
+				$(this).remove();
+			});
 	});
 
 });
