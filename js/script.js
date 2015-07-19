593886a33662932bc7b5a6e21f4331c2322c0935
extra <extra0@mail.ru>
2015-07-19 12:17:37 +0300

    готовность главной 90%

--- js/script.js
+++ js/script.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
 
 	// выпадающее меню
 	$('.menu__item').hover(function(){
-		$(this).children('div').slideToggle(100);
+		$(this).find('.dropdown-menu__list').slideToggle(100);
 	});
 
 	// вызов слайдера
 	$('.slider__wrap').bxSlider({
 		controls: false,
-		// pause: 3000,
-		// auto: true,
+		pause: 3000,
+		auto: true,
 		infiniteLoop: true
 	});
 
@@ -28,6 +28,48 @@ $(document).ready(function(){
 
 	$('.goods__bestseller').hover(function(){
 		$(this).find(".goods__bestseller-image").toggleClass("_not-active");
+	});
+
+	// вращаем иконку в блоке интересного в футере
+	$('.intresting__changer').hover(function(){
+		$(this).toggleClass('_hover');
+	});
+
+	// открываем модальное окно входа / регистарции
+	$('.enter').click(function(){
+		$('body').prepend('<div class="wrap"></div>');
+		$('.modal__window').slideDown(500);
+		return false;
+	});
+
+	// закрываем модальное окно по клику вне его области
+	$(document).click(function(event) {
+	    if ($(event.target).closest(".modal__window").length) return;
+	            $('.modal__window').slideUp(500);
+	            $('.wrap').remove();
+	            indexClick = 0;
+	    event.stopPropagation();
+	});
+
+	// вызываем табы
+	$('#tabs').tabs({
+		active: 2,
+		show: { effect: "fade", duration: 800 }
+	});
+
+	// маска на телефоны
+	$('.mask').mask('+38 (999) 999-99-99');
+
+	//  стилизация чекбоксов и селектов
+	$('.checkbox').styler();
+
+	// меняем блоки по клику в интересном
+	var total = $('.intresting__inner').length,
+		index = $('.intresting__inner').index();
+
+	$('.intresting__changer').click(function(){
+
+		return false;
 	});
 
 });
