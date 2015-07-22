5b7600c79acff5e955b869f440641231b49478b2
Ростислав <extra0@mail.ru>
2015-07-22 14:28:16 +0300

    готовый вариант переключателя интересного v2

--- js/script.js
+++ js/script.js
@@ -120,18 +120,15 @@ $(document).ready(function() {
 		num = block.length;
 
 	$('.intresting__changer').click(function(event) {
-		
 		if (i < num) {
 			block.eq(i - 1).removeClass('active');
 			block.eq(i).toggleClass('active');
-			console.log(i);
 			i++;
 		} else {
 			i = 1;
 			block.eq(0).addClass('active');
 			block.eq(-1).removeClass('active');
 		}
-		
 		return false;
 	});
 
